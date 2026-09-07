c452921efd1386baaa645f4a547309b3c991eea3
Alfin Dwi <alfindwi563@gmail.com>
2026-09-07 22:15:14 +0700

    [P-2] setup routing resource dan tugas admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route Praktikum
+Route::resource('books', BookController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
+Route::resource('members', MemberController::class);
+Route::resource('loans', LoanController::class);
+Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])->name('loans.kembalikan');
+
+// Route Tugas (Group Prefix /admin)
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Ini adalah halaman informasi khusus Admin.';
+    });
 });
